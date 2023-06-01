1b20f5d7dff880ab85e59f9fb797b38c53944e3a
pankv1 <121317229+pankv1@users.noreply.github.com>
2023-06-01 20:06:56 +0300

    render api update

--- api/products.js
+++ api/products.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://delivery-app-ylxl.onrender.com';
 
 // Получить список всех товаров
 export const fetchProducts = async () => {
